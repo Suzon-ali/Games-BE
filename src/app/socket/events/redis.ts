d47d6be0a2878094e84c2ba18aa11094089e068a
sujonali <sujon.dmf@gmail.com>
2025-10-30 12:27:06 +0600

    no-logged in user socket connection issue fixed

--- src/app/socket/events/redis.ts
+++ src/app/socket/events/redis.ts
@@ -8,11 +8,16 @@ export const initRedisSubscribers = (io: Server) => {
     else console.log(`📨 Subscribed to 'latestBets' (${count} channels)`);
   });
 
+  redisSubscriber.subscribe('newMessage', (err, count) => {
+    if (err) console.error('❌ Redis subscription error:', err);
+    else console.log(`💬 Subscribed to 'newMessage' (${count} channels)`);
+  });
+
   redisSubscriber.on('message', (channel, message) => {
     try {
       const parsed = JSON.parse(message);
       if (channel === 'latestBets') io.emit('latestBets', parsed);
-      if (channel === 'newMessage') io.emit('newMessage', parsed);
+      if (channel === 'newMessage') io.emit('sent:Message', parsed);
     } catch (err) {
       console.error('❌ Error parsing Redis message:', err);
     }
@@ -26,14 +31,17 @@ export const initRedisSubscribers = (io: Server) => {
 
   redisSubscriber.psubscribe('user:bet:placed:*', (err, count) => {
     if (err) console.error('Redis psubscribe error:', err);
-    else console.log(`📨 Subscribed to 'user:bet:placed:*' (${count} channels)`);
+    else
+      console.log(`📨 Subscribed to 'user:bet:placed:*' (${count} channels)`);
   });
 
   redisSubscriber.on('pmessage', (pattern, channel, message) => {
     try {
       const parsed = JSON.parse(message);
-      if (pattern === 'wallet:update:*' && parsed?.userId) io.to(parsed.userId).emit('wallet:update', parsed);
-      if (pattern === 'user:bet:placed:*' && parsed?.userId) io.to(parsed.userId).emit('user:bet:placed', parsed);
+      if (pattern === 'wallet:update:*' && parsed?.userId)
+        io.to(parsed.userId).emit('wallet:update', parsed);
+      if (pattern === 'user:bet:placed:*' && parsed?.userId)
+        io.to(parsed.userId).emit('user:bet:placed', parsed);
     } catch (err) {
       console.error('❌ Error parsing Redis pmessage:', err);
     }
